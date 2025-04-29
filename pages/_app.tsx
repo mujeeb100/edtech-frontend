1e99f41f7ab8e87e9b41292808dc67c39ac1fc26
mujeeb100 <muzeeb100@yahoo.co.in>
2025-04-29 23:55:21 +0530

    fixed the changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,7 @@
-// import React from 'react';
-// import { UserProvider } from '../src/context/useUserStore'; // Adjust the path if needed
-// import '../src/globals.css'; // Import global styles
-
-// function MyApp({ Component, pageProps }: any) {
-//   return (
-//     <UserProvider>
-//       <Component {...pageProps} />
-//     </UserProvider>
-//   );
-// }
-
 // export default MyApp;
 import React from 'react';
-import { UserProvider } from '../src/context/useUserStore'; // Adjust the path if needed
-import '../src/globals.css'; // Import global styles
+import { UserProvider } from '../src/context/useUserStore'; 
+import '../src/app/global.css'; 
 
 function MyApp({ Component, pageProps }: any) {
   return (
