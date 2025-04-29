df3ae11464d62b23eb6c660586a4694883836347
mujeeb100 <muzeeb100@yahoo.co.in>
2025-04-30 00:02:19 +0530

    has been fixed the app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import { UserProvider } from '../src/context/useUserStore'; 
 import '../src/app/globals.css'; 
+import type { AppProps } from 'next/app'; // Import proper types
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <UserProvider>
       <Component {...pageProps} />
     </UserProvider>
   );
-}
+}   
 
 export default MyApp;
